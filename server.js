51271fe67a0c4ddd5dae7a11f9c0e2eda76efe49
JannethE-Matute <84471343+JannethE-Matute@users.noreply.github.com>
2026-04-28 18:11:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor en puerto ${PORT}`);
 
-  // El comando de FFmpeg
-  // IMPORTANTE: Pon tu URL de lista.txt de Archive.org y tu Stream Key
   //const comando = `ffmpeg -re -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls -i "TU_URL_DE_LISTA_TXT" -c:a aac -b:a 128k -f flv "RTMP_DE_TU_RADIO/TU_STREAM_KEY"`;
   const comando = `ffmpeg -re -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls -i "TU_URL_DE_LISTA_TXT" -loop 1 -c:a aac -b:a 128k -f flv "RTMP_DE_TU_RADIO/TU_STREAM_KEY"`;
 
