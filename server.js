812b052ffa16a13951544448828515ae6d061462
JannethE-Matute <84471343+JannethE-Matute@users.noreply.github.com>
2026-04-29 15:51:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.listen(PORT, () => {
   console.log(`Servidor en puerto ${PORT}`);
 
   //const comando = `ffmpeg -re -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls -i "TU_URL_DE_LISTA_TXT" -c:a aac -b:a 128k -f flv "RTMP_DE_TU_RADIO/TU_STREAM_KEY"`;
-  const comando = `ffmpeg -re -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls -i "TU_URL_DE_LISTA_TXT" -loop 1 -c:a aac -b:a 128k -f flv "RTMP_DE_TU_RADIO/TU_STREAM_KEY"`;
+  const comando = `ffmpeg -re -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls -i "https://raw.githubusercontent.com/JannethE-Matute/Bode---Radio/refs/heads/main/canciones.txt" -loop 1 -c:a aac -b:a 128k -f flv "RTMP_DE_TU_RADIO/TU_STREAM_KEY"`;
 
   exec(comando, (err) => {
     if (err) console.error("Error FFmpeg:", err);
