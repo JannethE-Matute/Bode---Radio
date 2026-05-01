5f380e1dd29f7b83f386681326747107a78d8d73
JannethE-Matute <84471343+JannethE-Matute@users.noreply.github.com>
2026-05-01 12:24:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.listen(PORT, () => {
   
   const listaUrl = "https://raw.githubusercontent.com/JannethE-Matute/Bode---Radio/refs/heads/main/canciones.txt";
 
-  // 3. El comando con la imagen añadida (-loop 1 -i "${logoUrl}")
   const comando = `ffmpeg -re -loop 1 -i "${logoUrl}" -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls -i "${listaUrl}" -c:v libx264 -preset veryfast -b:v 1000k -maxrate 1000k -bufsize 2000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 128k -shortest -f flv "rtmp://live.twitch.tv/app/live_1489678025_u0KCUb4rSy0GwllQOhwdeg1SAOimfs"`;
 
   exec(comando, (err) => {
